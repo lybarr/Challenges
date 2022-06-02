9be01db5955929193bf3e5cb4897b45f44fcca4e
Lysette <lysette409@revature.net>
2022-06-02 11:34:52 -0500

    returnLongestString works.

--- src/main/java/Level0/ReturnLongestString/ReturnLongestString.java
+++ src/main/java/Level0/ReturnLongestString/ReturnLongestString.java
@@ -2,6 +2,14 @@ package Level0.ReturnLongestString;
 
 public class ReturnLongestString {
     public String longest(String[] s){
-        return null;
+        int max = 0;
+        int indexTracker = 0;
+        for(int i = 0; i < s.length; i++){
+            if(s[i].length() > max){
+                max = s[i].length();
+                indexTracker = i;
+            }
+        }
+        return s[indexTracker];
     }
 }
