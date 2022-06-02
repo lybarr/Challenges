f859825be70b6388a95894799e24dfd4be43b78d
Lysette <lysette409@revature.net>
2022-06-02 11:56:28 -0500

    level 1 updated.

--- src/main/java/Level1/Prime/Prime.java
+++ src/main/java/Level1/Prime/Prime.java
@@ -1,7 +1,19 @@
 package Level1.Prime;
-
+// optimized solution from geeksforgeeks
 public class Prime {
-    public boolean isPrime(int n){
-        return false;
+
+    // "Instead of checking till n, we can check till √n because a larger factor of n must be
+    // a multiple of smaller factor that has been already checked."
+
+    public boolean isPrime(int n) {
+        // Corner case
+        if (n <= 1) return false;
+
+        // Check from 2 to square root of n
+        for (int i = 2; i*i <= n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
     }
 }
